1fe2f443210ebd8e30ee0da5e9ec379dbfdf0bc0
George Raafat <georgexraafat@gmail.com>
2024-04-21 01:25:53 +0200

    working on review

--- scripts/bookPage.js
+++ scripts/bookPage.js
@@ -1,10 +1,67 @@
 const borrowButton = document.getElementById("borrow-button");
 const editButton = document.getElementById("edit-button");
 if (user.role === "admin") {
-    borrowButton.innerText = "Delete";
-    borrowButton.className = "delete-button";
+  borrowButton.innerText = "Delete";
+  borrowButton.className = "delete-button";
 } else {
-    editButton.remove();
+  editButton.remove();
 }
 
-//TODO: retrieve reviews from the database and display them on the page
+let url = new URL(window.location.href);
+let bookId = url.searchParams.get("id");
+const allBooks = JSON.parse(localStorage.getItem("books"));
+const book = allBooks.find((book) => book.id == bookId);
+
+const addReviewElement = document.querySelector(".add-review a");
+addReviewElement.href = `/HTML-Pages/review-book.html?id=${book.id}`;
+
+let categories = "";
+book.categories.forEach((category) => {
+  categories += `<li>${category}</li>`;
+});
+
+const bookDetails = document.querySelector(".book-details");
+const bookCover = document.querySelector(".book-cover img");
+bookCover.src = book.image;
+
+const data = `
+<div class="book-name-rating">
+<span class="book-name"><b>${book.title}</b></span><span
+  style="margin-left: 28px" class="rating">Rating: ${book.rating}</span>
+</div>
+<div class="author">
+<b>Author : </b>
+${book.author}
+</div>
+
+<div class="story-description">
+<b>Story: </b>
+
+<p class="story-paragraph">
+    ${book.story}
+</p>
+</div>
+<div class="category">
+<p>Categories:</p>
+<ul>
+    ${categories}
+</ul>
+</div>
+`;
+
+bookDetails.innerHTML = data;
+
+
+const reviewsElement = document.querySelector(".reviews");
+
+let reviews = "";
+book.reviews.forEach((review) => {
+    reviews += `
+    <div class="review">
+        <span class="username">${review.userName}:</span>
+        <span class="rating">Rating: ${review.score}/5</span>
+        <p class="review-content">${review.reviewContent}</p>
+    </div>
+    `;
+});
+reviewsElement.innerHTML = reviews;
