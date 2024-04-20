332024287abdcf2a1ac6e8ef07822e60aa6dc960
George Raafat <georgexraafat@gmail.com>
2024-04-21 00:23:03 +0200

    working on review script

---
+++ scripts/review.js
@@ -0,0 +1,23 @@
+let url = new URL(window.location.href);
+let bookId = url.searchParams.get("id");
+const allBooks = JSON.parse(localStorage.getItem("books"));
+const book = allBooks.find((book) => book.id == bookId);
+console.log(book);
+console.log(bookId);
+
+const scoreElement = document.getElementById("score");
+const reviewContentElement = document.getElementById("review-content");
+
+document.getElementById("reviewForm").onsubmit = (event) => {
+  event.preventDefault();
+  const review = {
+    userName: user.userName,
+    score: scoreElement.value,
+    reviewContent: reviewContentElement.value,
+  };
+
+  book.reviews.push(review);
+
+  localStorage.setItem("books", JSON.stringify(allBooks));
+  window.location.href = `/HTML-Pages/book-page.html?id=${book.id}`;
+};
