763ace3745ab5b88a169469ab1fade7a990fb375
ammarmorad24 <3mmarmorad2.am@gmail.com>
2024-04-20 18:54:40 +0200

    feat: adjusting the Book Page based on role

--- scripts/bookPage.js
+++ scripts/bookPage.js
@@ -2,9 +2,16 @@ const borrowButton = document.getElementById("borrow-button");
 const editButton = document.getElementById("edit-button");
 if (user.role === "admin") {
     borrowButton.innerText = "Delete";
-    borrowButton.style.backgroundColor = '#d62727';
-    //TODO: add a style for hover color too - #b62020
-}
-else {
+    borrowButton.style.backgroundColor = '#d62727'; // Red color
+    borrowButton.style.transition = "background-color 0.3s ease"; // Smooth transition
+    borrowButton.addEventListener("mouseenter", function() {
+        // Change background color on hover
+        borrowButton.style.backgroundColor = '#b62020'; // Darker red on hover
+    });
+    borrowButton.addEventListener("mouseleave", function() {
+        // Revert background color on mouse leave
+        borrowButton.style.backgroundColor = '#d62727'; // Original red color
+    });
+} else {
     editButton.remove();
 }
